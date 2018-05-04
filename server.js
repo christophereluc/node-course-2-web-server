806fcaf74dd7bf26548cc000378290b227a838d8
Christopher Luc <christopher.luc.contractor@hilton.com>
2018-05-03 22:34:06 -0700

    Modified port usage for heroku and added start command

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
 
+const port = process.env.port || 3000
+
 var app = express()
 
 hbs.registerPartials(__dirname + "/views/partials")
@@ -50,6 +52,6 @@ app.get('/bad', (req, res) => {
     errorMessage: "Error handling request"
   })
 })
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 })
